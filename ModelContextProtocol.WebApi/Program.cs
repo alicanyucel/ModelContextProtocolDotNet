f5595c8936843655fb1b3c3f9a803a3a66702c1c
Ali Can Yücel <yucelalican@hotmail.com>
2026-03-17 14:34:45 +0300

    mcp

--- ModelContextProtocol.WebApi/Program.cs
+++ ModelContextProtocol.WebApi/Program.cs
@@ -1,16 +1,13 @@
 using ModelContextProtocol.WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddTransient<WeatherService>();
+builder.Services.AddMcpServer().WithHttpTransport().WithToolsFromAssembly();
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -20,6 +17,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
-
+app.MapGet("/", () => "hello world");
+app.MapMcp("/mcp");
 app.Run();
